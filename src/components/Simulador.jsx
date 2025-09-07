17c887ddfc863a21555d93da2277fffa45ecc793
Redmetec <redmetec@gmail.com>
2025-09-07 14:39:48 -0500

    Update Simulador.jsx

--- src/components/Simulador.jsx
+++ src/components/Simulador.jsx
@@ -50,7 +50,7 @@ const Simulador = () => {
 
   const handleSubmit = async e => {
     e.preventDefault();
-    const res = await fetch('http://127.0.0.1:8000/calcular', {
+    const res = await fetch('https://back-metec.onrender.com/calcular', {
       method: 'POST',
       headers: { 'Content-Type': 'application/json' },
       body: JSON.stringify(formData)
